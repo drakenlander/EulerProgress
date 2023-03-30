6b4bb5a1f2bcf7f95dcd7783031866d33bea99bf
drakenlander <javierignacio0212@gmail.com>
2023-03-30 00:11:51 -0600

    Formatting +

--- 20.py
+++ 20.py
@@ -3,7 +3,7 @@
 Factorial Digit Sum
 Problem 20
 
-n! means n × (n − 1) × ... × 3 × 2 × 1
+n! means n × (n − 1) × ... × 3 × 2 × 1.
 
 For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
 and the sum of the digits in the number 10! is
